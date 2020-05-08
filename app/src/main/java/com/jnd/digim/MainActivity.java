5dfc0ed1d9c26f47b0dbc4513571395d2ef32cf0
jaydholakiya <jaydholakiya01@gmail.com>
2020-05-08 13:42:53 +0530

    Crash fixed for the vertical layout of dashboard activity(Idea fragment).

--- app/src/main/java/com/jnd/digim/MainActivity.java
+++ app/src/main/java/com/jnd/digim/MainActivity.java
@@ -26,6 +26,13 @@ import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -170,6 +177,7 @@ public class MainActivity extends AppCompatActivity {
     public void signOut() {
         FirebaseAuth.getInstance().signOut();
     }
+
 //    public void getIdeas() {
 //        FirebaseDatabase db = FirebaseDatabase.getInstance();
 //        DatabaseReference myRef = db.getReference("Ideas");
