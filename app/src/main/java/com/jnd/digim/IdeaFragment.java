f173cc5ab240877412c1f0042b2e2a144d16b1a8
jaydholakiya <jaydholakiya01@gmail.com>
2020-05-09 00:40:53 +0530

    Designed promote layout partially.

--- app/src/main/java/com/jnd/digim/IdeaFragment.java
+++ app/src/main/java/com/jnd/digim/IdeaFragment.java
@@ -1,10 +1,11 @@
 package com.jnd.digim;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
 public class IdeaFragment extends Fragment {
     @Nullable
--- app/src/main/java/com/jnd/digim/PromoteFragment.java
+++ app/src/main/java/com/jnd/digim/PromoteFragment.java
@@ -1,18 +1,111 @@
 package com.jnd.digim;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.valueOf;
+
 public class PromoteFragment extends Fragment {
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.promote_fragment,container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.promote_fragment, container, false);
+        final Spinner spinner = (Spinner) view.findViewById(R.id.promotionsList);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.promotions_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(parent.getContext(), "You selected nothing.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        Spinner spinners = (Spinner) view.findViewById(R.id.promotionsList);
+//        ArrayAdapter<CharSequence> adapters = ArrayAdapter.createFromResource(this.getContext(), a, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(final AdapterView<?> parent, View view, int position, long id) {
+//                String promotion = parent.getItemAtPosition(position).toString();
+//                Toast.makeText(parent.getContext(), promotion, Toast.LENGTH_SHORT).show();
+//                FirebaseDatabase db = FirebaseDatabase.getInstance();
+//                DatabaseReference myRef = db.getReference("Promotion list/" + promotion);
+//                myRef.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        List<String> promotions = new ArrayList<String>();
+//                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                            promotions.add(postSnapshot.getValue().toString());
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//                        Toast.makeText(parent.getContext(), "" + databaseError.toException(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                Toast.makeText(parent.getContext(), "You selected nothing.", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        return view;
     }
+
+//    public void getServices() {
+//        FirebaseDatabase db = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = db.getReference("Promotion list/" + promotion);
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                ArrayList<Object> promotions = new ArrayList<>();
+//                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                    String data = postSnapshot.getValue(String.class);
+//                    promotions.add(data);
+//                    Toast.makeText(parent.getContext(), "" + promotions, Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                Toast.makeText(parent.getContext(), "" + databaseError.toException(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 }
